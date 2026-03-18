d2cd5adc40da6a92b2f70f75c7f1e920a8d165ff
ohjh9701 <ohjh4875@gmail.com>
2026-03-18 09:26:41 +0900

    [추가] 꿀템 프로젝트 - 루트 연결 및 비즈니스 작업 시작

--- ggultem/src/router/root.jsx
+++ ggultem/src/router/root.jsx
@@ -4,12 +4,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Loading from "../pages/Loading";
 
 const Main = lazy(() => import("../pages/MainPage"));
+const Business = lazy(() => import("../pages/Business/MainPage"));
 const root = createBrowserRouter([
+  /* ===== 메인페이지 연결 영역 ===== */
   {
     path: "/",
     element: (
       <Suspense fallback={<Loading />}>
         <Main />
+      </Suspense>
+    ),
+  },
+  /* ===== 비즈니스 연결 영역 ===== */
+  {
+    path: "/business",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Business />
       </Suspense>
     ),
   },
